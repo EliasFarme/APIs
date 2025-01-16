5b27f9e5cf9b81536338049f9ecf4ad1276957f6
Elias Pereira <elias.pereira@farme.com.br>
2025-01-16 10:31:48 -0300

    Hoje utilizamos o path para uma validação a mais para nossa classe

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from fastapi.responses import JSONResponse
 from fastapi import Response
+from fastapi import Path
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
@@ -29,7 +30,7 @@ async def get_cursos():
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(default=None, title= 'ID do curso ', description='Deve ser entre 1 e 2', gt=0, lt=3)):
     try:
         curso = cursos[curso_id]
         return curso
